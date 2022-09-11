4313cb29c29b9feafb909f44b43c9472987d8bc9
DionKanhai <dionkanhais@gmail.com>
2022-09-11 21:05:43 +0000

    add a function that checks the ISS passtimes for a spefific location

---
+++ index.js
@@ -0,0 +1,48 @@
+//Import the following functions from the iss.js file
+const { nextISSTimesForMyLocation } = require('./iss');
+
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
+
+
+// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// fetchCoordsByIP('76.71.25.233', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log("It worked! Returned coordinates:", coordinates);
+// });
+
+// fetchISSFlyOverTimes(({ latitude: '42.3149367', longitude: '-83.0363633' }), (error, orbits) => {
+    
+//     if (error) {
+//       console.log("It didn't work!" , error);
+//       return;
+//     }
+//     console.log("It worked! Returned ISS passes:", orbits);
+// });
